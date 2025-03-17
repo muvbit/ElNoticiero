c12dfd6fc99684be8d1b662815e189b8e7ee8960
muvbit <alberola@outlook.com>
2025-03-17 21:08:56 +0100

    Version 1.6.2  No background on BottomNav's item selected  No bottom menu on Settings Fragment

--- app/src/main/java/com/muvbit/elnoticiero/fragments/SettingsFragment.kt
+++ app/src/main/java/com/muvbit/elnoticiero/fragments/SettingsFragment.kt
@@ -3,10 +3,13 @@ package com.muvbit.elnoticiero.fragments
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 import com.muvbit.elnoticiero.R
+import com.muvbit.elnoticiero.activities.MainActivity
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        val activity = requireActivity() as MainActivity
+        activity.binding.bottomNav.menu.clear()
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
 }
