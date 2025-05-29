8f883046e7bd0aa90cfae3ca7ff2df177cd805fc
muvbit <alberola@outlook.com>
2025-05-29 10:56:46 +0200

    Version 1.9  Final version candidate.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 1
-        versionName = "1.8.9"
+        versionName = "1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
